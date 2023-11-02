1474a8421e163382db841c0874d2accab5632f8f
minju <kimminju3335@naver.com>
2023-11-02 18:12:16 +0900

    Read(ALL)기능 구현 + Bootstrap 적용

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,9 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# app_name = 'articles'
+app_name = 'articles'
 
-# # urlpatterns = [
-# #     path('', views.index, name='index'),
+urlpatterns = [
+    path('', views.index, name='index'),
 
-# # ]
+]
